6753092e90159310a2e7e3ee41b798b76a6db5f4
Huy-DNA <huydo862003@gmail.com>
2024-09-15 20:59:32 +0700

    feat: basic framework

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 # typed: true
 # frozen_string_literal: true
 
+require 'sorbet-runtime'
 require 'selenium-webdriver'
 
-def fetch_all_items(driver)
+extend T::Sig
 
+class Item < T::Struct
+  const :name, String
+  const :sub_items, T::Array[Item]
+  const :metadata, T::Hash[String, T.anything]
+end
+
+sig { params(driver: Selenium::WebDriver::Chrome::Driver).returns(T::Array[Item]) }
+def fetch_all_items_with(driver)
+  driver.find_elements(css: '.box-item').map do |e|
+    Item.new(name: '', sub_items: [], metadata: {})
+  end
 end
 
 def main
@@ -15,6 +27,8 @@ def main
   driver.navigate.to 'https://www.thegioiic.com/product/'
 
   puts 'Navigated to www.thegioiic.com'
+
+  fetch_all_items_with driver
 
   puts 'End session'
 
